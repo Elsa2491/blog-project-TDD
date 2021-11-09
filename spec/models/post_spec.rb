970b5a8bc49235b5aaa235042fbe28cbe0d72185
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-09 20:27:09 +0100

    add guard-rspec and test to increment comment_count

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,9 +3,23 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
+  context 'comment association' do
+    it 'should increment comments_count' do
+      post = Post.new(name: "Coffee Machine kata solved")
+      post.comments << Comment.new(content: "Demo")
+      post.save
+      expect(Comment.count).to eq(1)
+      expect(post.comments_count).to eq(1)
+    end
+  end
+
+  # Description
   it "should persist a post" do
+    # Act
     Post.create(name: "Difference between .tr and .sgub")
     Post.create(name: "Coffee Machine kata solved")
+
+    # Assert
     expect(Post.count).to eq(2)
   end
 end
