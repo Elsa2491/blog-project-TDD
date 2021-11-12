4a4846cb48c2be36ee675f40c3b09363d49ffdce
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-12 19:32:38 +0100

    add length validation on user first_name, and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, through: :posts
 
-  validates :first_name, presence: true
-  validates :email, presence: true
+  validates :first_name, presence: true, length: { maximum: 50 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
