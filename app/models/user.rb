cdbb06cafa1e2624b30a2f33ef90cf8361bf0f9e
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-12 20:11:01 +0100

    add test email validation with regex

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   has_many :comments, through: :posts
 
   validates :first_name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maximum: 255 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+            format: { with: VALID_EMAIL_REGEX }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -29,4 +29,22 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
+  test "email validation should accept valid addresses" do
+    valid_adresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn]
+    valid_adresses.each do |valid_address|
+      @user.email = valid_address
+      assert @user.valid?, "#{valid_address.inspect} should be valid"
+    end
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_adresses = %w[user@example,com user_at_foo.org user_name@example.
+      foo@bar_baz.com foo@bar+baz.com]
+    invalid_adresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
+  end
+
 end
