7fcf898b8af0e4d2c32412b53b8fb89d8422d81f
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-15 23:45:10 +0100

    convert all email to email.downcase, add index on email in DB

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,9 +1,9 @@
 FactoryBot.define do
   factory :user do
-    first_name { "MyString" }
-    last_name { "MyString" }
-    nickname { "MyString" }
-    email { "example@email.com" }
-    password { "MyString" }
+      first_name { "MyString" }
+      last_name { "MyString" }
+      nickname { "MyString" }
+      email { "example@email.com" }
+      password { "MyString" }
   end
 end
