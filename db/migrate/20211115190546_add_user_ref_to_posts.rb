827e24b0367916038034ab9fb00e24fcdc821ad3
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-15 20:09:09 +0100

    try to fix bug

--- db/migrate/20211115190546_add_user_ref_to_posts.rb
+++ db/migrate/20211115190546_add_user_ref_to_posts.rb
@@ -1,4 +1,4 @@
-class AddUserReferenceToPost < ActiveRecord::Migration[6.0]
+class AddUserRefToPosts < ActiveRecord::Migration[6.0]
   def change
     add_reference :posts, :user, null: false, foreign_key: true
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it 'should create a user' do
+    # expect(User.count).to eq (1)
     user = build(:user)
     user.save
     expect(User.count).to eq(1)
