1d524e921edfb123d784015d9f1243f5fef88ab5
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-17 17:26:06 +0100

    add body to Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_221548) do
+ActiveRecord::Schema.define(version: 2021_11_17_162135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_221548) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.text "body"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
