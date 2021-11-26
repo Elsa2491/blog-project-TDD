2d354df594ea22ebbdbd20d6c2d844c6fc877b5c
Elsa Touma elsa.touma@gmail.com <elsa.touma@gmail.com>
2021-11-26 18:52:13 +0100

    login and navbar and bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,11 +5,18 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
+
+  # def current_user
+  #   @current_user ||= User.find_by(id: session[:user_id])
+  #   # equivalent to @current_user = @current_user|| User.find_by(id: session[:user_id])
+  # end
 
   def destroy
   end
